2bb74cabf6ec180f4fa93c40143fe1adeea9c9ad
ya2s34 <115533136+ya2s34@users.noreply.github.com>
2023-04-26 15:00:20 +0200

    Add files via upload

---
+++ lord_of_geek/lord_of_geek/App/controleur/c_passerCommande.php
@@ -0,0 +1,32 @@
+<?php
+
+include 'App/modele/M_commande.php';
+
+/**
+ * Controleur pour les commandes
+ * @author Loic LOG
+ */
+switch ($action) {
+
+    case 'passerCommande':
+
+        $n = nbJeuxDuPanier();
+        if ($n > 0) {
+
+            if (isset($idClient) && !empty($idClient)) {
+                $lesIdJeu = getLesIdJeuxDuPanier();
+                try {
+                    M_Commande::creerCommande($idClient, $lesIdJeu);
+                    afficheMessage("Commande passée avec succès !");
+                    supprimerPanier();
+                } catch (PDOException $e) {
+                    echo 'erreur lors de la commande';
+                }
+            }
+        } else {
+            afficheMessage("Panier vide !!");
+            $uc = '';
+        }
+        break;
+}
+
